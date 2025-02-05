1967070ae5897902d818b1df9a99b1161866db6f
Vivan Patange <101522498+Vivan24@users.noreply.github.com>
2025-02-05 01:56:13 -0500

    Added comments, cleaned up code

--- src/most_similar_books.py
+++ src/most_similar_books.py
@@ -17,55 +17,76 @@ def format_similar_message(source_book_title,
              f'also liked {most_similar_title}. (Score = {similarity_score:.3f})\n'
     return result
 
+
+def make_matrix(ratings_path, books_path):
+    """
+    Reads through ratings.txt and books.txt files simultaneously and produces
+    a 2D array of all user ratings for each book as well as a normal list for
+    all book names and returns two tuples for both values (matrix & book names)
+    """
+    
+    # initializes arrays
+    matrix = []
+    user_vector = []
+    book_names = []
+
+    # opens to read both ratings and book name files and adds data to arrays
+    with ratings_path.open("r") as scores, books_path.open("r") as books:
+        for ratings, names in zip_longest(scores, books, fillvalue=''):
+            # formatted to append all data for current user before moving to next
+            if ratings.strip().startswith("User"):
+                matrix.append(user_vector)
+                user_vector = []
+            else:
+                user_vector.append(int(ratings.strip()))
+            book_names.append(names.strip())
+        
+        # cleans up array and adds last user data
+        matrix.pop(0)
+        matrix.append(user_vector)
+
+    return matrix, book_names
+
 def main():
     """
     For each book in books.txt, this program outputs to the file
     most-similar-books.txt the book and the book that is most similar
     to it given the cosine similarity between it and all other books
     using the ratings in ratings.txt. This program does not prompt
-    the user for any input. For more details, see the homework assignment.
+    the user for any input.
     """
 
-    ratings_matrix = []
-    user_vector = []
-    book_names = []
+    # pulls ratings and book names from function
+    ratings_matrix, list_books = make_matrix(config.DATA_RATINGS, config.DATA_BOOKS)
 
-    # stores all ratings of users in a matrix while saving all book names into an array
-    with config.DATA_RATINGS.open("r") as scores, config.DATA_BOOKS.open("r") as books:
-        for ratings, names in zip_longest(scores, books, fillvalue=''):
-            if ratings.strip().startswith("User"):
-                ratings_matrix.append(user_vector)
-                user_vector = []
-            else:
-                user_vector.append(int(ratings.strip()))
-
-            book_names.append(names.strip())
-        
-        ratings_matrix.append(user_vector)
-        ratings_matrix.pop(0)
-
-    # comparing books to one another
+    # catches error if there aren't two or more books
     if len(ratings_matrix[0]) < 2:
         print("ERROR: There must be two or more books' ratings for each user to compare in \"raitings.txt\" data file")
     else:
+        # matrix for tracking name of most similar book and similarity value from comparisions
         similarity_matrix = []
         for i in range(len(ratings_matrix[0])):
             most_similar_score = -1
             for j in range(len(ratings_matrix[0])):
+                # avoids comparing to itself
                 if i != j:
+                    # each colum represents a book in matrix
                     curr_col = [row[i] for row in ratings_matrix]
                     diff_col = [row[j] for row in ratings_matrix]
-                    curr_score = similarity(curr_col, diff_col)
+                    curr_score = similarity(curr_col, diff_col) # compare logic
 
+                    # updates value until highest similarity score is saved
                     if curr_score > most_similar_score:
                         most_similar_score = curr_score
-                        most_similar_book = book_names[j]
+                        most_similar_book = list_books[j] # j is column index for each book
 
+            # before moving to next book, adds most similar book's name and score
             similarity_matrix.append([most_similar_book, most_similar_score])
 
+    # writes to the output file
     with config.DATA_OUTPUT.open("w") as output:
         for k in range(len(similarity_matrix)):
-            output.write(format_similar_message(book_names[k], similarity_matrix[k][0], similarity_matrix[k][1]))
+            output.write(format_similar_message(list_books[k], similarity_matrix[k][0], similarity_matrix[k][1]))
 
 if __name__ == "__main__":
     main()
